930776ae39443ad6f0c437bd4734aa19d40e2339
Vitor Klein <vitor@kdatabi.com>
2023-10-05 20:43:36 -0300

    change service names and routes

---
+++ src/api/auth-service.js
@@ -0,0 +1,99 @@
+import API from "./api";
+
+export const registerUser = async (name, email, password) => {
+  try {
+    const userData = {
+      name,
+      email,
+      password,
+      roles: "ROLE_USER",
+    };
+    const response = await API.post(`auth/register`, userData);
+    return response.userData;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw error;
+    } else {
+      throw new Error("Erro durante o cadastro do usuário.");
+    }
+  }
+};
+
+export const getUserToken = async (email, password) => {
+  try {
+    const loginData = {
+      email,
+      password,
+    };
+    const response = await API.post(`auth/create-token`, loginData);
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw error;
+    } else {
+      console.log(error);
+      throw new Error("Erro durante login.");
+    }
+  }
+};
+
+export const isTokenValid = async (userToken) => {
+  try {
+    return await API.get(`auth/validate-token?token=${userToken}`);
+    // template
+    // const requestParam = 'paramValue';
+    // const requestHeader = {
+    //   'Custom-Header': 'headerValue',
+    // };
+    // const requestBody = {
+    //   key1: 'value1',
+    //   key2: 'value2',
+    // };
+    // axios.post(apiUrl, requestBody, {
+    //   params: {
+    //     param: requestParam,
+    //   },
+    //   headers: requestHeader,
+    // })
+  } catch (error) {
+    throw new Error("Error fetching user data", error);
+  }
+};
+
+export const forgotPassword = async (email) => {
+  try {
+    const headers = {
+      "Content-Type": "application/json",
+      "Custom-Email-Header": email,
+    };
+    const response = await API.post(`auth/forgot-password`, { headers });
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw error;
+    } else {
+      console.log(error);
+      throw new Error("Erro durante recuperação de senha.");
+    }
+  }
+};
+
+export const updatePassword = async (userToken, userPassword) => {
+  try {
+    const apiHeaders = {
+      "Content-Type": "application/json",
+      "Custom-Password-Header": userPassword,
+    };
+    const apiParams = {
+      token: userToken,
+    };
+    const response = await API.post(`auth/update-password`, null, {
+      headers: apiHeaders,
+      params: apiParams,
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Erro durante atualização da senha!");
+  }
+};
--- src/api/user-service.js
+++ src/api/user-service.js
@@ -1,8 +1,5 @@
 import API from "./api";
 
-const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ2aXRvckBrZGF0YWJpLmNvbSIsImlhdCI6MTY4Mjg4MjQ3NSwiZXhwIjoxNjgyODgzOTE1fQ.ksaWSagmDm4BsEVVCoZld6HX8hF88k6kzZrWQGqwPY4";
-
 export const getUserData = async (userId) => {
   try {
     const response = await API.get(`users/${userId}`, {
@@ -13,42 +10,5 @@ export const getUserData = async (userId) => {
     return response.data;
   } catch (error) {
     throw new Error("Error fetching user data", error);
-  }
-};
-
-export const getUserToken = async (email, password) => {
-  try {
-    const loginData = {
-      email,
-      password,
-    };
-    const response = await API.post(`users/create-token`, loginData);
-    return response.data;
-  } catch (error) {
-    if (error.response && error.response.data) {
-      throw error;
-    } else {
-      console.log(error);
-      throw new Error("Erro durante login.");
-    }
-  }
-};
-
-export const registerUser = async (name, email, password) => {
-  try {
-    const userData = {
-      name,
-      email,
-      password,
-      roles: "ROLE_USER",
-    };
-    const response = await API.post(`users/register`, userData);
-    return response.userData;
-  } catch (error) {
-    if (error.response && error.response.data) {
-      throw error;
-    } else {
-      throw new Error("Erro durante o cadastro do usuário.");
-    }
   }
 };
