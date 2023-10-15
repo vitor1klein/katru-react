51c1c6abfb1ba57e2312a566a53b3021dfe0767d
Vitor Klein <vitor@kdatabi.com>
2023-10-14 21:53:39 -0300

    updating policies

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require("express");
 const { resolve } = require("path");
 const app = express();
 
-// Define your CSP policy as a string
 const cspPolicy =
-  "default-src 'self'; style-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://unpkg.com/leaflet@1.7.1/dist/leaflet.css 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='; img-src 'unsafe-inline' 'self' data:; child-src 'none'; font-src 'unsafe-inline' 'self' https://fonts.googleapis.com/ https://fonts.gstatic.com/; connect-src 'unsafe-inline' https://portal-katru-backend-stg-45b34fb09c5c.herokuapp.com";
+  "default-src 'self'; style-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://unpkg.com/leaflet@1.7.1/dist/leaflet.css 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='; img-src 'self' data:; child-src 'none'; font-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://fonts.gstatic.com/; connect-src 'self' https://portal-katru-backend-stg-45b34fb09c5c.herokuapp.com";
 
 // Middleware to set the CSP header for all routes
 app.use((req, res, next) => {
