efc522585eecdcc92847a82c274930080f69be23
Vitor Klein <vitor@kdatabi.com>
2023-10-14 23:50:05 -0300

    updating header content

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const { resolve } = require("path");
+const helmet = require("helmet");
 const app = express();
 
-const cspPolicy =
-  "default-src 'self'; style-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://unpkg.com/leaflet@1.7.1/dist/leaflet.css 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='; img-src 'self' data:; child-src 'none'; font-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://fonts.gstatic.com/; connect-src 'self' https://portal-katru-backend-stg-45b34fb09c5c.herokuapp.com";
+// const cspPolicy =
+//   "default-src 'self'; style-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://unpkg.com/leaflet@1.7.1/dist/leaflet.css 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='; img-src 'self' data:; child-src 'none'; font-src 'unsafe-inline' 'self' https://fonts.googleapis.com https://fonts.gstatic.com/; connect-src 'self' https://portal-katru-backend-stg-45b34fb09c5c.herokuapp.com";
 
-// Middleware to set the CSP header for all routes
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", cspPolicy);
-  next();
-});
+// // Middleware to set the CSP header for all routes
+// app.use((req, res, next) => {
+//   res.setHeader("Content-Security-Policy", cspPolicy);
+//   next();
+// });
+
+// Configure the Content-Security-Policy header using the helmet-csp middleware
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+      styleSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        "https://fonts.googleapis.com",
+        "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css",
+        "'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='",
+      ],
+      imgSrc: ["'self'", "data:"],
+      connectSrc: ["'self'", "https://portal-katru-backend-stg-45b34fb09c5c.herokuapp.com"],
+      fontSrc: ["'self'", "https://fonts.googleapis.com", "https://fonts.gstatic.com"],
+    },
+  })
+);
 
 app.use("/", express.static(resolve(__dirname, "./build")));
 
